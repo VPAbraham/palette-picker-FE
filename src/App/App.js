a65a7adcb6493e6d289fd2559cc12666279c4f61
Quinne Farenwald <q.farenwald@gmail.com>
2019-12-08 13:33:19 -0700

    Add white x and white plus to modal

--- src/App/App.js
+++ src/App/App.js
@@ -3,7 +3,8 @@ import Modal from 'react-modal';
 import './App.scss';
 import Nav from '../Nav/Nav';
 import Palette from '../Palette/Palette';
-import blackPlus from '../assets/images/plus_black.svg';
+import whiteClose from '../assets/images/close_white.svg';
+import whitePlus from '../assets/images/plus_white.svg';
 // Modal.setAppElement('#yourAppElement')
 
 class App extends Component {
@@ -48,15 +49,18 @@ class App extends Component {
           className="Modal"
           contentLabel="Example Modal"
         >
-          <img src={blackPlus} onClick={this.closeModal} alt="black plus symbol" className="modalplus"/>
+          <img src={whiteClose} onClick={this.closeModal} alt="white x symbol" className="modalClose"/>
           <section className="menu">
             <div className="menu-items">
+              <img src={whitePlus} alt="white plus symbol" className="modalPlus"/>
               <h1>SAVE PALETTE</h1>
             </div>
             <div className="menu-items">
+              <img src={whitePlus} alt="white plus symbol" className="modalPlus"/>
               <h1>CREATE NEW PROJECT</h1>
             </div>
             <div className="menu-items">
+              <img src={whitePlus} alt="white plus symbol" className="modalPlus"/>
               <h1>VIEW ALL PROJECTS</h1>
             </div>
           </section>
