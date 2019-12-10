aa0c7348ef16ff24ec0268d7f6c175dc3a700a9f
Quinne Farenwald <q.farenwald@gmail.com>
2019-12-09 19:36:07 -0700

    Delete palette from form and database

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -12,7 +12,7 @@ const Projects = (props) => {
         return palette.projects_id === project.id
       })
       let palmap = specPal.map((pal) => {
-        return <div className="palList"><h4>{pal.name.toUpperCase()}</h4><button className="modal-delete">DELETE</button></div>
+        return <div className="palList"><h4>{pal.name.toUpperCase()}</h4><button className="modal-delete" onClick={() => deletePalette(pal.id)}>DELETE</button></div>
       })
       return <div><div className="projList"><h2>{project.name.toUpperCase()}</h2><button className="modal-delete">DELETE</button></div>{palmap}</div>
     })
