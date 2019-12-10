1f3455614d59dfeb20c5e9de19265deaf263a04f
Quinne Farenwald <q.farenwald@gmail.com>
2019-12-09 18:02:38 -0700

    Display palettes for each project

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -2,17 +2,24 @@ import React from 'react';
 import './Projects.scss';
 
 const Projects = (props) => {
-const projects = props.projects
+  const projects = props.projects
+  const palettes = props.palettes
 
-  const getProjectNames = () => {
+  const displayProjects = () => {
     return projects.map((project) => {
-      return <h2>{project.name.toUpperCase()}</h2>
+      let specPal = palettes.filter((palette) => {
+        return palette.projects_id === project.id
+      })
+      let palmap = specPal.map((pal) => {
+        return <h4>{pal.name.toUpperCase()}</h4>
+      })
+      return <div><h2>{project.name.toUpperCase()}</h2>{palmap}</div>
     })
   }
 
   return (
     <section className="Projects">
-      {getProjectNames()}
+      {displayProjects()}
     </section>
   )
 }
