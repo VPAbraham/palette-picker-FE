93774d9923699d0d49a32722244930621b045686
Quinne Farenwald <q.farenwald@gmail.com>
2019-12-11 18:41:35 -0700

    Write update state test for color3 color4 and color5

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -58,4 +58,61 @@ describe('App', () => {
 
     expect(wrapper.state('color2')).toEqual('#ff6767')
   })
+
+  it('should update color3 in state when refreshPalettes is called', () => {
+
+    let wrapper = shallow(<App />)
+
+    const mockPalette = {
+    "name": "Fall",
+    "projects_id": 25,
+    "color1": "#3CCB84",
+    "color2": "#697689",
+    "color3": "#3CCB84",
+    "color4": "#697689",
+    "color5": "#3CCB84"
+  }
+
+    wrapper.instance().refreshPalettes(mockPalette)
+
+    expect(wrapper.state('color3')).toEqual('#ffe596')
+  })
+
+  it('should update color4 in state when refreshPalettes is called', () => {
+
+    let wrapper = shallow(<App />)
+
+    const mockPalette = {
+    "name": "Fall",
+    "projects_id": 25,
+    "color1": "#3CCB84",
+    "color2": "#697689",
+    "color3": "#3CCB84",
+    "color4": "#697689",
+    "color5": "#3CCB84"
+  }
+
+    wrapper.instance().refreshPalettes(mockPalette)
+
+    expect(wrapper.state('color4')).toEqual('#b3e6ff')
+  })
+
+  it('should update color5 in state when refreshPalettes is called', () => {
+
+    let wrapper = shallow(<App />)
+
+    const mockPalette = {
+    "name": "Fall",
+    "projects_id": 25,
+    "color1": "#3CCB84",
+    "color2": "#697689",
+    "color3": "#3CCB84",
+    "color4": "#697689",
+    "color5": "#3CCB84"
+  }
+
+    wrapper.instance().refreshPalettes(mockPalette)
+
+    expect(wrapper.state('color5')).toEqual('#41406d')
+  })
 })
