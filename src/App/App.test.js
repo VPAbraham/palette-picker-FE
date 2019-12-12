4c5f1c6f442098eb10adcf94847816b90f144073
Quinne Farenwald <q.farenwald@gmail.com>
2019-12-11 18:37:05 -0700

    Fix snapshot and write update state test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -10,8 +10,33 @@ it('renders without crashing', () => {
 });
 
 describe('App', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    let wrapper = shallow(<App />)
+  })
+
   it('should match the snapshot with all data passed in', () => {
     let wrapper = shallow(<App />)
     expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should update palettes in state when refreshPalettes is called', () => {
+
+    let wrapper = shallow(<App />)
+
+    const mockPalette = {
+    "name": "Fall",
+    "projects_id": 25,
+    "color1": "#3CCB84",
+    "color2": "#697689",
+    "color3": "#3CCB84",
+    "color4": "#697689",
+    "color5": "#3CCB84"
+  }
+
+    wrapper.instance().refreshPalettes(mockPalette)
+
+    expect(wrapper.state('color1')).toEqual('#baddc3')
   })
 })
