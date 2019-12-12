d867cefeec6649b0cc81352a34471ab7700049ee
Quinne Farenwald <q.farenwald@gmail.com>
2019-12-11 18:38:55 -0700

    Write update state test for color2

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -21,7 +21,7 @@ describe('App', () => {
     expect(wrapper).toMatchSnapshot();
   })
 
-  it('should update palettes in state when refreshPalettes is called', () => {
+  it('should update color1 in state when refreshPalettes is called', () => {
 
     let wrapper = shallow(<App />)
 
@@ -38,5 +38,24 @@ describe('App', () => {
     wrapper.instance().refreshPalettes(mockPalette)
 
     expect(wrapper.state('color1')).toEqual('#baddc3')
+  })
+
+  it('should update color2 in state when refreshPalettes is called', () => {
+
+    let wrapper = shallow(<App />)
+
+    const mockPalette = {
+    "name": "Fall",
+    "projects_id": 25,
+    "color1": "#3CCB84",
+    "color2": "#697689",
+    "color3": "#3CCB84",
+    "color4": "#697689",
+    "color5": "#3CCB84"
+  }
+
+    wrapper.instance().refreshPalettes(mockPalette)
+
+    expect(wrapper.state('color2')).toEqual('#ff6767')
   })
 })
